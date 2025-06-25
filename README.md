35df8810a6ddf1bb06b8af659475cbd862b34be3
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2025-06-25 07:53:35 +0300

    feat/feat-add-docker-config

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# laravelDevhunt5
+# 🔄 RDP — Laravel + Docker + Redis
+
+Ce projet `RDP` est une application Laravel configurée pour tourner dans des conteneurs Docker avec Redis comme moteur de cache, et une base de données SQLite (ou MySQL, selon configuration).
+
+---
+
+## 🧰 Prérequis
+
+Avant de démarrer, assure-toi d’avoir installé :
+
+- [Docker](https://www.docker.com/)
+- [Docker Compose](https://docs.docker.com/compose/)
+- [Git](https://git-scm.com/)
+
+---
+
+## 🚀 Lancement rapide
+
+### 1. Cloner le projet
+
+```bash
+git clone git@github.com:DiaryFenohasina/RDP-CycleTrafficLights.git
+cd RDP
+docker-compose up -d --build
+
---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "Starting entrypoint script..."
+
+# Vérifier si Redis est installé
+if command -v redis-server > /dev/null; then
+    echo "Redis server found at: $(which redis-server)"
+    echo "Starting Redis server..."
+    redis-server /etc/redis/redis.conf &
+    sleep 2
+    if redis-cli ping > /dev/null; then
+        echo "Redis is running successfully"
+    else
+        echo "Redis failed to start"
+        exit 1
+    fi
+else
+    echo "Redis server not found"
+    exit 1
+fi
+
+# Créez le fichier de log avec les permissions correctes
+#touch /var/log/artisan_cron.log
+#chmod 666 /var/log/artisan_cron.log
+touch /var/www/html/storage/logs/laravel.log
+chown www-data:www-data /var/www/html/storage/logs/laravel.log
+
+chmod -R 777 /var/www/html/storage
+
+mkdir /var/www/html/app/public
+chown -R  www-data:www-data /var/www/html/app/public
+chmod -R 775 /var/www/html/app/public
+
+# Configurez le cron pour exécuter tous les jours à 00h15
+#echo "15 0 * * * php /var/www/html/artisan app:notifier-batch-draft >> /var/log/artisan_cron.log 2>&1" | crontab -
+#echo "Cron configuré pour exécuter à 00h15 tous les jours."
+
+# Démarrez le service cron
+#service cron start
+#echo "Service cron démarré."
+
+# Lancer le programme des tâches Laravel
+nohup php artisan schedule:work > /dev/null 2>&1 &
+
+# Lancer Apache en mode foreground
+exec apache2-foreground
